95eb4e098ee61fb6f1d6c9e5f779a2c7f0894153
rulifarel <160815275+rulifarel@users.noreply.github.com>
2025-12-11 23:10:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ async function run() {
   // a valid username and password! Note that in a production environment,
   // you do not want to store your password in plain-text here.
   const uri =
-    "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+    "mongodb+srv://<rulifarelti23_db_user>:<nmZ7pTJM4GlXHjzK>@<cluster-url>?retryWrites=true&w=majority";
 
   // The MongoClient is the object that references the connection to our
   // datastore (Atlas, for example)
